4321609b3c328a0b59c7d257d8921167bdd5b4c4
S. M. Sakil Imran <sakilcse7@gmail.com>
2014-11-27 22:03:35 +0600

    Now we can set different divs where multiple chart for any single page
    If we want to implement multiple chart in one page then it would be better to set just the **div_id** for each chart. So we don't need to define that in onstructor everytime

--- Lib/GoogleCharts.php
+++ Lib/GoogleCharts.php
@@ -107,7 +107,7 @@ class GoogleCharts extends Object
 
         if (!empty ($div))
         {
-            $this->div = $div;
+            $this->div = $this->setDiv($div);
         }
 
     }
@@ -163,6 +163,11 @@ class GoogleCharts extends Object
 
         return false;
 
+    }
+    
+    public function setDiv($divId = 'chart_div')
+    {
+        $this->div = $divId;
     }
 
 }
